f967de2ebf0481f3d0ae3c80ba25dbee8e2ec1c4
Emiliano Gutiérrez <eeggaa19@hotmail.com>
2020-04-22 16:31:41 -0500

    Add Back to steep 1 and validate description

--- app/src/main/java/com/example/myappointments/CreateAppointmentActivity.kt
+++ app/src/main/java/com/example/myappointments/CreateAppointmentActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myappointments
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,8 +21,12 @@ class CreateAppointmentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_appointment)
 
         btnNext.setOnClickListener {
-            cvStep1.visibility = View.GONE
-            cvStep2.visibility = View.VISIBLE
+            if(etDescription.text.toString().length < 3) {
+                etDescription.error = getString(R.string.validate_appointment_description)
+            } else {
+                cvStep1.visibility = View.GONE
+                cvStep2.visibility = View.VISIBLE
+            }
         }
 
         btnConfirmAppointment.setOnClickListener {
@@ -104,4 +109,24 @@ class CreateAppointmentActivity : AppCompatActivity() {
 
     private fun Int.twoDigits()
             = if (this>=10) this.toString() else  "0$this"
+
+    override fun onBackPressed() {
+        if(cvStep2.visibility == View.VISIBLE) {
+            cvStep2.visibility = View.GONE
+            cvStep1.visibility = View.VISIBLE
+        } else if (cvStep1.visibility == View.VISIBLE) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.dialog_create_appointment_exit_title))
+            builder.setMessage(getString(R.string.dialog_create_appointment_exit_message))
+            builder.setPositiveButton(getString(R.string.dialog_create_appointment_exit_positive_btn)) { _, _ ->
+                finish()
+            }
+            builder.setNegativeButton(getString(R.string.dialog_create_appointment_exit_negative_btn)) { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            builder.show()
+        }
+    }
 }
