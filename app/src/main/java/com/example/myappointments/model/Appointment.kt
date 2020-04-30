6bb068a0b8f8305eb43d0c9ac7de0f8cce5ff483
Emiliano Gutiérrez <eeggaa19@hotmail.com>
2020-04-30 14:42:35 -0500

    Read Appointments from API

--- app/src/main/java/com/example/myappointments/model/Appointment.kt
+++ app/src/main/java/com/example/myappointments/model/Appointment.kt
@@ -1,8 +1,34 @@
 package com.example.myappointments.model
 
+import com.google.gson.annotations.SerializedName
+
+/*-
+"id": 1,
+        "description": "Tengo dolor de cabeza",
+        "scheduled_date": "2020-04-09",
+        "type": "Examen",
+        "created_at": "2020-04-04 01:28:31",
+        "status": "Cancelada",
+        "scheduled_time_12": "9:00: AM",
+        "specialty": {
+            "id": 3,
+            "name": "Neurología"
+        },
+        "doctor": {
+            "id": 3,
+            "name": "Emiliano Gutiérrez D"
+        }
+-*/
 data class Appointment (
     val id: Int,
-    val doctorName: String,
-    val scheduledDate: String,
-    val scheduledTime: String
+    val description: String,
+    val type: String,
+    val status: String,
+
+    @SerializedName("scheduled_date") val scheduledDate: String,
+    @SerializedName("scheduled_time_12") val scheduledTime: String,
+    @SerializedName("created_at") val createdAt: String,
+
+    val specialty: Specialty,
+    val doctor: Doctor
 )
--- app/src/main/java/com/example/myappointments/ui/AppointmentsActivity.kt
+++ app/src/main/java/com/example/myappointments/ui/AppointmentsActivity.kt
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myappointments.R
+import com.example.myappointments.io.ApiService
 import com.example.myappointments.model.Appointment
+import com.example.myappointments.util.PreferenceHelper
+import com.example.myappointments.util.PreferenceHelper.get
+import com.example.myappointments.util.toast
 import kotlinx.android.synthetic.main.activity_appointments.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.ArrayList
 
 class AppointmentsActivity : AppCompatActivity() {
+
+    private val apiService : ApiService by lazy {
+        ApiService.create()
+    }
+
+    private val preferences by lazy {
+        PreferenceHelper.defaultPrefs(this)
+    }
+
+    private val appointmentAdapter = AppointmentAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointments)
 
-        val appointments = ArrayList<Appointment>()
+        loadAppointments()
+
+        /*-val appointments = ArrayList<Appointment>()
         appointments.add(
             Appointment(1,"Dr. Gutiérrez Arias", "12/12/2021", "14:00 PM")
         )
@@ -23,11 +42,33 @@ class AppointmentsActivity : AppCompatActivity() {
         )
         appointments.add(
             Appointment(3,"Dr. Otro", "14/12/2021", "16:00 PM")
-        )
+        )-*/
 
         rvAppointments.layoutManager = LinearLayoutManager(this) //GridLayoutManager
-        rvAppointments.adapter =
-            AppointmentAdapter(appointments)
+        rvAppointments.adapter = appointmentAdapter //AppointmentAdapter(appointments)
 
+    }
+
+    private fun loadAppointments(){
+        val jwt = preferences["jwt",""]
+        val call= apiService.getAppointments("Bearer $jwt")
+        call.enqueue(object: Callback<ArrayList<Appointment>>{
+            override fun onFailure(call: Call<ArrayList<Appointment>>, t: Throwable) {
+                toast(t.localizedMessage)
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<Appointment>>,
+                response: Response<ArrayList<Appointment>>
+            ) {
+                if (response.isSuccessful){
+                    response.body() ?.let {
+                        appointmentAdapter.appointments = it
+                        appointmentAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
+        })
     }
 }
